e85f3d39fd1d0473dcde8c98be683c2326703ba0
Ariel Flahaut <111144387+arielflt@users.noreply.github.com>
2022-11-23 14:20:38 +0100

    Generator project

---
+++ Generator/Generator/generator.cpp
@@ -0,0 +1,8 @@
+//
+//  generator.cpp
+//  Generator
+//
+//  Created by Ariel Flahaut on 23/11/2022.
+//
+
+#include "generator.hpp"
---
+++ Generator/Generator/generator.hpp
@@ -0,0 +1,13 @@
+//
+//  generator.hpp
+//  Generator
+//
+//  Created by Ariel Flahaut on 23/11/2022.
+//
+
+#ifndef generator_hpp
+#define generator_hpp
+
+#include <stdio.h>
+
+#endif /* generator_hpp */
