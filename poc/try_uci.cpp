01a01d0176b4d3390ec781bc4ab8808faf5fab38
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2022-11-24 08:40:37 +0100

    feat: try stockfish uci connection

---
+++ poc/try_uci.cpp
@@ -0,0 +1,21 @@
+/**
+ * try_uci.cpp -- Try calling stockfish using UCI protocol
+ *
+ */
+
+#include <vector>
+
+class Board {
+    // Placeholder class to store chessboard
+};
+
+class Move {
+    // Placeholder class to store a single chess move
+}
+
+class UCICommunicator {
+  public:
+    std::vector<Move> retrieve_moves(Board b) { ; }
+};
+
+int main() {}
