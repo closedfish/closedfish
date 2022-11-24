880374242a2453a205f0d4ed7c6c06c8144056cd
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2022-11-24 09:50:28 +0100

    feat: try popen

--- poc/try_uci.cpp
+++ poc/try_uci.cpp
@@ -3,6 +3,8 @@
  *
  */
 
+#include <cstdio>
+#include <iostream>
 #include <vector>
 
 class Board {
@@ -11,11 +13,35 @@ class Board {
 
 class Move {
     // Placeholder class to store a single chess move
-}
+};
 
 class UCICommunicator {
   public:
-    std::vector<Move> retrieve_moves(Board b) { ; }
+    // ref:
+    // https://stackoverflow.com/questions/6171552/popen-simultaneous-read-and-write
+    UCICommunicator(const char *stockfish_path) {
+        stockfish = popen(stockfish_path, "w");
+        if (stockfish == NULL) {
+            std::cerr << "Stockfish cannot be opened" << std::endl;
+            exit(1);
+        }
+    }
+    ~UCICommunicator() { pclose(stockfish); }
+    std::vector<Move> retrieve_moves(Board b) {
+        const int ST_MAX = 512;
+        char st[ST_MAX];
+        while (fgets(st, ST_MAX, stockfish) != NULL) {
+            std::cout << st << std::endl;
+        }
+        fputs("help", stockfish);
+        fflush(stockfish);
+        while (fgets(st, ST_MAX, stockfish) != NULL) {
+            std::cout << st << std::endl;
+        }
+    }
+
+  private:
+    FILE *stockfish;
 };
 
-int main() {}
+int main() { UCICommunicator comm("./Stockfish/src/stockfish"); }
