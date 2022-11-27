c37eb9389f424dd8cc01a339eee1ec725b01893a
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2022-11-27 01:28:02 +0100

    feat: initial try_interface skeleton

---
+++ poc/try_interface.cpp
@@ -0,0 +1,24 @@
+/**
+ * try_interface.cpp -- Try importing stockfish "directly"
+ *
+ */
+
+#include "../Board Implementation/CFBoard.cpp"
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+class Move {
+    // Placeholder class to store a single chess move
+};
+
+std::vector<Move> get_stockfish_output(CFBoard cfb) {
+    // to be implemented
+    ;
+}
+
+int main() {
+    // load board
+    // fetch stockfish output for this board
+    // output
+}
