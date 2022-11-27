61f5675bdc2d38d240575305bf027ab37656aaa2
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2022-11-27 02:34:22 +0100

    feat: board conversion

--- poc/try_interface.cpp
+++ poc/try_interface.cpp
@@ -13,14 +13,21 @@
  * @brief converts closedfish board to stockfish board
  *
  * @param cfb closedfish board
- * @return Position stockfish board
+ * @param pos stockfish board
+ * @param states stockfish statelist pointer
  */
-Position convert_CFBoard_to_Stockfish_Position(CFBoard cfb) {
-    // to be implemented
+void convert_CFBoard_to_Stockfish_Position(CFBoard cfb,
+                                           Stockfish::Position &pos,
+                                           Stockfish::StateListPtr &states) {
+    Stockfish::Position::init(); // if not yet init (should rather be inside the
+                                 // main function)
+    std::string fen = cfb.toFEN();
+    states = Stockfish::StateListPtr(new std::deque<StateInfo>(1));
+    pos.set(fen, false, &states->back(), Stockfish::Threads.main());
 }
 
 // look at uci.cpp for reference
-void call_stockfish(Position &pos, StateListPtr &states,
+void call_stockfish(Stockfish::Position &pos, Stockfish::StateListPtr &states,
                     Search::LimitsType limits, bool ponderMode) {
     limits.startTime = now();
     Threads.start_thinking(pos, states, limits, ponderMode);
