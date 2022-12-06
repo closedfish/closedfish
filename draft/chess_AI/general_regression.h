866e4f77a6d13128dd57c02435b01ebae7ee56aa
Cassidy-Kevorkian <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2022-12-06 22:46:42 +0100

    full regression algo (minus some small details)

--- draft/chess_AI/general_regression.h
+++ draft/chess_AI/general_regression.h
@@ -1,9 +1,48 @@
 #pragma once
-namespace regression1
-{
-    //add methods
-    int test()
+#include "C:\Users\Cassi\Downloads\eigen-3.4.0\eigen-3.4.0\Eigen\Dense"
+
+namespace Regression1 {
+    
+    //This function creates the matrix for the regression from the function basis Basis (a pointer) and dimension size of the basis.
+    //we have that the datapoint coordinates are stored in the pointer X of a list of length num_data_points 
+    Eigen::MatrixXd setUpQ(double* Basis, double** X, int dimension, int num_data_points)
     {
-        return 69;
+        Eigen::MatrixXd Q(num_data_points, dimension);
+
+        //for (int i = 0; i < dimension; i++){
+        //    for (int j = 0; j < num_data_points; j++){
+
+        //        Q(dimension, num_data_point) = Basis[i](X[j]); 
+          //  }
+        //}
+
+        return Q;
     }
+
+    //create the vector Y (the output obained for the data points)
+    Eigen::VectorXd setUpYVect(double* data_outputs, int num_data_points){
+        Eigen::VectorXd Y(num_data_points);
+        for (int i = 0; i < num_data_points; i++){
+            Y(i) = data_outputs[i];
+        }
+
+        return Y;
+    }
+    //this is the best linear combination of the basis functions to get the best fit in the form of 
+    //a vector of length dimension
+    //m here is the size of the vectors contained in X (14 <= m <= 16)
+    Eigen::VectorXd bestFitF(double* Basis, double** X, double* data_outputs, int dimension, int num_data_points, int m){
+
+        Eigen::MatrixXd Q = setUpQ(Basis, X, dimension, num_data_points);
+        Eigen::VectorXd Y = setUpYVect(data_outputs, num_data_points);
+
+        Eigen::MatrixXd trans_Q = Q.transpose();
+        Eigen::MatrixXd helper_M = (trans_Q * Q).inverse();
+
+        return helper_M * trans_Q * Y;
+
+    }
+
+
+
 }
--- draft/chess_AI/main.cpp
+++ draft/chess_AI/main.cpp
@@ -3,5 +3,11 @@
 
 int main()
 {
-    std::cout<<regression1::test();
+    Eigen::MatrixXd m(2, 2);
+    m(0, 0) = 1;
+    m(0, 1) = 2;
+    m(1, 0) = 3;
+    m(1, 1) = 4;
+    //std::cout<<regression1::test() << std::endl;
+    std::cout << m << std::endl;
 }
