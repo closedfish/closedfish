74a28ea7d58c3821a17dbcc8fdf414eda5e5aea3
andreibornea <andreybornea@gmail.com>
2022-12-08 09:33:10 +0100

    Import Nlohmann for openings and fix building bugs

---
+++ build/app/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/andre/OneDrive/Desktop/Closedfish/closedfish/app
+# Build directory: C:/Users/andre/OneDrive/Desktop/Closedfish/closedfish/build/app
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
