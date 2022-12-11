56a61e72199433a202c85c86fe0f73b58bb9fc96
Cassidy-Kevorkian <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2022-12-11 14:11:30 +0100

    4 ideas of basis fully implemented

--- src/play/general_regression.h
+++ src/play/general_regression.h
@@ -1,6 +1,6 @@
 #pragma once
-//#include "C:\Users\Cassi\Downloads\eigen-3.4.0\eigen-3.4.0\Eigen\Dense"
-#include <Eigen/Dense>
+#include "C:\Users\Cassi\Downloads\eigen-3.4.0\eigen-3.4.0\Eigen\Dense"
+//#include <Eigen/Dense>
 
 //This is  the funtion class that will enables us to have an array of functions.
 //
@@ -155,52 +155,55 @@ class Func
 
 
 
-// namespace TheRegression {
+namespace TheRegression {
     
-//     //This function creates the matrix for the regression from the function basis Basis (a pointer) and dimension size of the basis.
-//     //we have that the datapoint coordinates are stored in the pointer X of a list of length num_data_points 
+    //This function creates the matrix for the regression from the function basis Basis (a pointer) and dimension size of the basis.
+    //we have that the datapoint coordinates are stored in the pointer X of a list of length num_data_points 
     
-//     Eigen::MatrixXd setUpQ(Funk* basis, int** X, int dimension, int num_data_points)
-//     {
-//         Eigen::MatrixXd Q(num_data_points, dimension);
-
-//         for (int i = 0; i < dimension; i++){
-//             for (int j = 0; j < num_data_points; j++){
-
-//                 Q(dimension, num_data_points) = basis[i].SquareDistance(1, 2); 
-//             }
-//         }
-
-//         return Q;
-//     }
-
-//     //create the vector Y (the output obained for the data points)
-//     Eigen::VectorXd setUpYVect(double* data_outputs, int num_data_points){
-//         Eigen::VectorXd Y(num_data_points);
-//         for (int i = 0; i < num_data_points; i++){
-//             Y(i) = data_outputs[i];
-//         }
-
-//         return Y;
-//     }
-//     //this is the best linear combination of the basis functions to get the best fit in the form of 
-//     //a vector of length dimension
-//     //m here is the size of the vectors contained in X (14 <= m <= 16)
-//     Eigen::VectorXd bestFitF(Funk* basis, double** X, double* data_outputs, int dimension, int num_data_points, int m){
-
-//         Eigen::MatrixXd Q = setUpQ(basis, X, dimension, num_data_points);
-//         Eigen::VectorXd Y = setUpYVect(data_outputs, num_data_points);
-
-//         Eigen::MatrixXd trans_Q = Q.transpose();
-//         Eigen::MatrixXd helper_M = (trans_Q * Q).inverse();
-
-//         return helper_M * trans_Q * Y;
-
-//     }
+    Eigen::MatrixXd setUpQ(Func* basis, int** X, int dimension, int num_data_points)
+    {
+        Eigen::MatrixXd Q(num_data_points, dimension);
 
 
 
-// }
+        for (int j = 0; j < dimension; j++){
+            for (int i = 0; i < num_data_points; i++){
+
+                Q(i, j) = basis[i].Eval(X[2 * j], X[2 * j + 1]);
+            }
+        }
+
+        return Q;
+    }
+
+    //create the vector Y (the output obained for the data points)
+    Eigen::VectorXd setUpYVect(double* data_outputs, int num_data_points){
+        Eigen::VectorXd Y(num_data_points);
+        for (int i = 0; i < num_data_points; i++){
+            Y(i) = data_outputs[i];
+        }
+
+        return Y;
+    }
+
+    //this is the best linear combination of the basis functions to get the best fit in the form of 
+    //a vector of length dimension
+
+    Eigen::VectorXd bestFitF(Func* basis, int** X, double* data_outputs, int dimension, int num_data_points){
+
+        Eigen::MatrixXd Q = setUpQ(basis, X, dimension, num_data_points);
+        Eigen::VectorXd Y = setUpYVect(data_outputs, num_data_points);
+
+        Eigen::MatrixXd trans_Q = Q.transpose();
+        Eigen::MatrixXd helper_M = (trans_Q * Q).inverse();
+
+        return helper_M * trans_Q * Y;
+
+    }
+
+
+
+}
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 
 namespace SqrtDifBasis
