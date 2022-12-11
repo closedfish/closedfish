66cf46048f62120be269f44c37a06bee91e46ef3
Cassidy-Kevorkian <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2022-12-11 12:06:31 +0100

    structure update in src/play

--- src/play/PlayMain.cpp
+++ src/play/PlayMain.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
-
-#include "PlayMain.hpp"
 #include "closenessAI.h"
 
-
-void print_hello_world() { std::cout << "Hello World!\n"; }
-int main1()
+int main()
 {
     Eigen::MatrixXd m(2, 2);
     m(0, 0) = 1;
     m(0, 1) = 2;
     m(1, 0) = 3;
     m(1, 1) = 4;
-    Funk f(2, 5);
 
     int l_1[8];
     int l_2[8];
-
+    Func* b = Basis1::GenerateBasis();
     for (int i = 0; i < 8; i ++)
     {
         l_1[i] = i;
-        l_2[i] = i - 1;
+        l_2[i] = i * i - 1;
     }
-
-
-     
-
-    std::cout<< f.average_pos(l_2, l_1) << std::endl;
-    
-    //std::cout << m << f.getval(4) << std::endl;
+    for (int i = 0; i < 23; i ++)
+    {
+        std::cout<< b[i].Eval(l_1, l_2) <<std::endl;
+    }
     return 0;
 }
