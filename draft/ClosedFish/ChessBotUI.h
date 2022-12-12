521e9c279d83df789f91fdd268e36bdf4b85e903
Razvan Puscasu <65716317+toadehu@users.noreply.github.com>
2022-12-12 22:31:53 +0100

    Updated to current version
    Added the new functions

--- draft/ClosedFish/ChessBotUI.h
+++ draft/ClosedFish/ChessBotUI.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <utility>
 
 struct piece
 {
@@ -22,32 +23,49 @@ struct piece
 	/// <summary>
 	/// Handle to the bitmap associated with the piece
 	/// </summary>
-	HBITMAP hbm;
+	//HBITMAP hbm;
 };
 
 class bmpClass
 {
 public:
+	/// <summary>
+	/// Class constructor
+	/// This object DOES NOT support reinitialization at the moment
+	/// It will cause a crash
+	/// </summary>
+	/// <param name="type_of_init">true for using opencv, false for testing</param>
 	bmpClass(bool type_of_init)
 	{
 		scrW = GetSystemMetrics(SM_CXSCREEN);
 		scrH = GetSystemMetrics(SM_CYSCREEN);
-		getPrimaryScreen(prevBmp);
-		if (type_of_init == false)//no openCV this is only for testing on chess.com
+		if (type_of_init == false)//this is only for testing on chess.com
 		{
 			boardL = 304 * scrW / 1920;
-			boardT = 137 * scrH / 1080;
-			sideL = 100 * scrH / 1080;
-			sideL += (sideL & 1);
+			boardT = 105 * scrH / 1080;
+			sideL = 99 * scrH / 1080;
+			//sideL += (sideL & 1);
 		}
 		else
 		{
 			//TBD
 		}
-		init_squares();
 		split_the_board();
-		AllocConsole();
 	}
+	/// <summary>
+	/// Default constructor, for some testing
+	/// </summary>
+	bmpClass() { bmpClass(false); }
+	///this causes undefined, unexpected behaviour and will cause a crash
+	~bmpClass()
+	{
+		for (int i = 0; i < 63; ++i)
+		{
+			DeleteObject(this->hSquares[i]);
+			//std::cout << i << "<-\n";
+			//hSquares[i] = NULL;
+		}
+	}//*/
 	/// <summary>
 	/// Send a click to a given screen position
 	/// </summary>
@@ -55,7 +73,15 @@ public:
 	/// <param name="posY"> y position</param>
 	/// <returns>true if the click was sent succesfully</returns>
 	static bool sendClick(int posX, int posY);
-	static void send_input(char* inps);
+	/// <summary>
+	/// old function, pretty useless now
+	/// </summary>
+	/// <param name="inps">chess inputs, format "a3a4" moves a piece from a3 to a4</param>
+	void send_input(char* inps);
+	/// <summary>
+	/// old function, useless atm
+	/// </summary>
+	/// <param name="bmp"></param>
 	static void getPrimaryScreen(BITMAP& bmp);
 	/// <summary>
 	/// Creates a bitmap and returns the path to the file
@@ -63,6 +89,10 @@ public:
 	/// <param name="filename">name of the file</param>
 	/// <returns>string representing the filepath</returns>
 	static char* _saveScreenToFile(LPCWSTR filename);
+	/// <summary>
+	/// Creates a bitmap and returns the path to the file
+	/// </summary>
+	/// <param name="filename">name of the file</param>
 	static void saveScreenToFile(LPCWSTR filename);
 	/// <summary>
 	/// Saves current screen to a file, with a given type
@@ -92,15 +122,23 @@ public:
 	/// <param name="data"></param>
 	void writeToFile(LPCWSTR filename, char* data);
 	/// <summary>
-	/// test
+	/// prints a square, mostly for testing
 	/// </summary>
-	void printSq();
-
-private:
+	void printSq(int id);
 	/// <summary>
-	/// Splits the board into 64 bitmaps, one for each square, name uses snakecase because it might cause a memory leak
+	/// Splits the board into 64 bitmaps, one for each square, 
+	/// name uses snakecase because it might cause a memory leak or other issuess
 	/// </summary>
 	void split_the_board();
+	/// <summary>
+	/// Gives (an educated guess to) the colours of the chessboard.
+	/// Chances are this works 100% of the time
+	/// </summary>
+	/// <param name  = "col1">first colour</param>
+	/// <param name  = "col2">second colour</param>
+	void getBoardColours(int &col1, int &col2);
+
+private:
 
 	/// <summary>
 	/// snakecase because it might cause a memory leak, or it may be useless
@@ -108,10 +146,9 @@ private:
 	void init_squares();
 
 	//this is a memory leak waiting to happen :/
-	BITMAP prevBmp;//somehow this doesn't cause a segfault or memory leak... yet
-	BITMAP squares[64];
 	HBITMAP hSquares[64];
-	piece pieces[32];
+
+	//piece pieces[32];
 	int scrW, scrH;
 	int boardL, boardT, sideL;
 };
