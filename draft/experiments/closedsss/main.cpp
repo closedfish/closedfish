e7c00ad05db4a40b9ca7ae14427bea8b153823ed
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2022-12-15 17:48:11 +0100

    fix: hash board

--- draft/experiments/closedsss/main.cpp
+++ draft/experiments/closedsss/main.cpp
@@ -3,10 +3,20 @@
 #include "../../../lib/board_implementation/naiveCheckCheck.cpp"
 #include <string>
 #include <tuple>
+#include <unordered_set>
 #include <vector>
+
+/**
+ * @brief Hash the board to avoid repetition
+ *
+ * @param board
+ * @return uint64_t
+ */
+std::string hash_board(CFBoard board) { return board.toFEN(); }
 
 class SearchTree {
   private:
+    std::unordered_set<std::string> hashes;
     class SearchTreeNode {
       private:
         CFBoard board;
@@ -24,10 +34,13 @@ class SearchTree {
          * @param steps_limit limit the number of steps
          * @return int the number of leaf nodes (i.e. breakthrough)
          */
-        int search(int &steps_limit) {
-            std::cout << "DBG " << steps_limit << std::endl;
+        int search(int &steps_limit, std::unordered_set<std::string> &hashes) {
+            // std::cout << "DBG " << steps_limit << std::endl;
             if (steps_limit <= 0)
                 return -1;
+            if (hashes.find(hash_board(board)) != hashes.end())
+                return 0;
+            hashes.insert(hash_board(board));
             int leaves = 0;
             std::vector<CFBoard> next_boards; // assume we have this
             // Sirawit: in this experiment, we don't care about the depth
@@ -41,15 +54,17 @@ class SearchTree {
                     int j = __builtin_ctzll(next_positions & -next_positions);
                     next_positions ^= 1ll << (uint64_t)j;
                     next_boards.push_back(board);
-                    std::cout << "Trying to move from " << i << " to " << j
-                              << std::endl;
-                    next_boards.back().movePiece(i, j);
-                    std::cout << "DONE" << std::endl;
-                    if (next_boards.back().getPieceFromCoords(j) == -1) {
+                    // std::cout << "Trying to move from " << i << " to " << j
+                    //           << std::endl;
+                    next_boards.back().removePiece(i);
+                    if (next_boards.back().getPieceFromCoords(j) != -1) {
                         size++;
                         leaves++;
                         next_boards.pop_back();
+                    } else {
+                        next_boards.back().addPiece(k, j);
                     }
+                    // std::cout << "DONE" << std::endl;
                 }
             }
             // END
@@ -59,7 +74,7 @@ class SearchTree {
             }
             for (SearchTreeNode *each : children) {
                 size--;
-                int current_leaves = each->search(--steps_limit);
+                int current_leaves = each->search(--steps_limit, hashes);
                 if (current_leaves < 0)
                     leaves = -1;
                 else
@@ -82,19 +97,17 @@ class SearchTree {
      * of leaves)
      */
     std::tuple<int, int, int> search(int steps_limit = 100000) {
-        int leaves = root->search(steps_limit);
+        int leaves = root->search(steps_limit, hashes);
         return std::make_tuple(root->height, root->size, leaves);
     }
 };
 
 int main() {
     // test on the initial default board
-    std::cout << "Hello, world!" << std::endl;
-    CFBoard init;
+    CFBoard init("3k4/8/7p/2p1p1pP/1pPpPpP1/1P1P1P2/N7/2K5 w - - 0 1");
     SearchTree tree(init);
-    std::cout << "OK" << std::endl;
     int a, b, c;
-    std::tie(a, b, c) = tree.search(5);
+    std::tie(a, b, c) = tree.search(80000);
     std::cout << a << " " << b << " " << c << std::endl;
     return 0;
 }
