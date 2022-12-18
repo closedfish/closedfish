37788c19499da0628298b0182135f3019930510c
etienne-leroy <medieval511@gmail.com>
2022-12-18 19:08:46 +0100

    feat
    Worked on CFBoard unit-tests

--- draft/board_tests/test_Board_Implementation.cpp
+++ draft/board_tests/test_Board_Implementation.cpp
@@ -3,26 +3,237 @@ using namespace std;
 
 namespace CFBoardTester {
 
-void test_pieceIdToChar();
-void test_pieceCharToId();
-void test_getColorBitBoard();
-void test_getPieceColorBitBoard();
-void test_getPieceBoardFromIndex();
-void test_getCurrentPlayer();
-void test_getPieceFromCoords();
-void test_getBit();
-void test_getMaterialCount();
-void test_addPiece();
-void test_removePiece();
-void test_movePiece();
-void test_forceUndo();
-void test_getCardinals();
-void test_getDiagonals();
-void test_getKnightPattern();
-void test_getKingPattern();
-void test_getPawnPattern();
-void test_getLegalMoves();
-void test_naiveCheckCheck();
+void test_pieceIdToChar(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = { // check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_pieceIdToChar ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //Implement
+    }
+}
+
+void test_pieceCharToId(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = { // check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_pieceCarToId ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+
+void test_getColorBitBoard(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = { // check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getColorBitBoard ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getPieceColorBitBoard(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = { // check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getPieceColorBitBoard ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getPieceBoardFromIndex(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_etPieceBoardFromIndex ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getCurrentPlayer(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getCurrentPlayer ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getPieceFromCoords(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getPieceFromCoords ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getBit(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getBit ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getMaterialCount(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getMaterialCount ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_addPiece(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_addPiece ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_removePiece(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_removePiece ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_movePiece(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_movePiece ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_forceUndo(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_forceUndo ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getCardinals(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getCardinals ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getDiagonals(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getDiagonals ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getKnightPattern(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getKnightPattern ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getKingPattern(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getKingPattern ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getPawnPattern(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = {// check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getPawnPattern ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+void test_getLegalMoves(){
+    std::vector<std::tuple<bool, bool, CFBoard>> tests = { // check test cases
+        std::make_tuple(false, false, CFBoard()),
+        std::make_tuple(true, true,
+                        CFBoard("rnbqkbnr/ppp1pppp/3p4/8/Q7/2P5/PP1PPPPP/"
+                                "RNBQKBNR w KQkq - 0 1"))};
+    std::cout << "test_getLegalMoves ";
+    std::cout << "[";
+    for (auto test : tests) {
+        //implement
+    }
+}
+
 void testNaiveCheckCheck() {
     std::vector<std::tuple<bool, bool, CFBoard>> tests = {
         std::make_tuple(false, false, CFBoard()),
