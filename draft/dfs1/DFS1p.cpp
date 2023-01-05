860d152448b54a3bc4a1b7db328c520b081ac713
AarryaSaraf <aarryasaraf@gmail.com>
2023-01-05 08:21:01 +0100

    Commit Repeat
    Previous commit not showing up. Testing with commit 2

--- draft/dfs1/DFS1p.cpp
+++ draft/dfs1/DFS1p.cpp
@@ -9,21 +9,33 @@ using namespace std;
 
 bool is_protected(int u)
 {
+	/*
+	* @brief: NOT IMPLEMENTED. Checks if pawn is protected
+	* @param <int> square index
+	* @return if pawn on square is protected
+	*/
 	return false;
 }
 
-//evaluates the position might be different from stockfish. to be implemented later on
+
 int eval()
 {
-	//gives the evaluation
+	/*
+	* @brief: NOT IMPLEMENTED. evaluates the position might be different from stockfish. to be implemented later on
+	* @param Board: <CFBoard> Board 
+	* @return naive evaluation of the board
+	*/
 	return 0;
 }
 
 //returns number of possible moves by every piece
 int numMoves(int n)
 {
-	//input piece id
-	// output total possible moves
+	/*
+	* @brief: gives total number of possible moves based on the piece index
+	* @param n: <int> piece index
+	* @return total possible moves
+	*/
 	if (n % 2 == 1)
 		n--;
 	if (n == 0)
@@ -41,15 +53,23 @@ int numMoves(int n)
 
 int newTile(int piece, int move)
 {
-	//input the piece id and the move id
-	//output the change in square
+	/*
+	* @brief: gives the change in tile of the move
+	* @param piece: <int> piece index
+	* @param move: <int> move index
+	* @return the change in square index
+	*/
 
-	if (numMoves(piece) < move)
+	// move is invalid
+	if (numMoves(piece) < move || move<0)
 		return 0;
+	// move is valid
 	else
 	{
+		// by piece index
 		switch (piece)
 		{
+			//pawn
 			if (piece % 2 == 1)
 				return (-1 * newTile(piece - 1, move));
 			int k = 0;
@@ -62,7 +82,7 @@ int newTile(int piece, int move)
 					k= - 7;
 				else if (move == 4|| move ==6)
 					k= - 9;
-
+			// knight
 			case 2:
 				if (move == 1)
 					k = 17;
@@ -74,7 +94,7 @@ int newTile(int piece, int move)
 					k = -15;
 				else
 					k = -1 * k;
-
+			// bishop
 			case 4:
 				if ( move < 7)
 					k = 7 * k;
@@ -84,7 +104,7 @@ int newTile(int piece, int move)
 					k = -9 * k;
 				else
 					k = 9 * k;
-
+			// rook
 			case 6:
 				if (move < 7)
 					k = -1 * k;
@@ -94,7 +114,7 @@ int newTile(int piece, int move)
 					k = 1 * k;
 				else
 					k = 8 * k;
-
+			// queen. Just does the corresponding bishop or rook move
 			case 8:
 				int d = (int)move / 7;
 				if (d % 2 == 0)
@@ -102,7 +122,7 @@ int newTile(int piece, int move)
 				else
 					move -= 7;
 				return (newTile(6, move - 7 * d / 2));
-
+			// king
 			case 10:
 				if (move == 1)
 					k = 7;
@@ -128,23 +148,6 @@ int newTile(int piece, int move)
 	}
 }
 
-bool isSquareInBoard(int sq)
-{
-	//input a square. 
-	//outputs true if its in the board
-	if (sq >= 0 && sq <= 63)
-		return true;
-	else
-		return false;
-}
-
-//checks if a move is stupid or not
-// a move is stupid 
-bool isGood(CFBoard& board, int sq, int move)
-{
-
-}
-
 /*
 * the main idea has been optimized a lot more than last week and to that end,
 * the DFS has been changed to a BFS and the ideas have been quite a lot
@@ -159,35 +162,39 @@ bool isGood(CFBoard& board, int sq, int move)
 void dfsMain(CFBoard board)
 {
 	/*
-	* input: the board
-	* output: the 3 best evaluations and the move lines
+	* @brief: does the DFS
+	* @param board: <CFBoard> current board.
+	* @return the top 3 lines and their eval
 	*/
-	
-}
 
-void dfsMain2(CFBoard board)
-{
+	// starting best evals and moves
 	int eval[3] = { 0,0,0 };
 	string play[3] = { "","","" };
+	// first dfs gets to dept 5
 	dfsHelp(board, "", 1, eval, play);
-	std::cout << eval << endl;
-	std::cout << play << endl;
+	// the next part of the dfs goes to depth 15
 	for (int i = 0; i < 3; i++)
 	{
+		//makes 3 boards with the best moves
+		CFBoard board1 = board;
+		CFBoard board2 = board;
+		CFBoard board3 = board;
+		//sets everything back to default
 		for (int j = 0; j < 3; j++)
 		{
 			eval[j] = 0;
 			play[j] = "";
 		}
-		CFBoard board1 = board;
-		CFBoard board2 = board;
-		CFBoard board3 = board;
+		//makes the 3 best boards
 		board1.makeMovess(play[0]);
 		board2.makeMovess(play[1]);
 		board3.makeMovess(play[2]);
+		//dfs for board 1
 		dfsHelp(board1, play[0], 1, eval, play);
+		// makes another copy of all the arrays
 		int evalc[3] = { 0,0,0 };
 		string playc[3] = { "","","" };
+		//updates the copy and resets the array
 		for (int j = 0; j < 3; j++)
 		{
 			evalc[j] = eval[j];
@@ -195,6 +202,7 @@ void dfsMain2(CFBoard board)
 			playc[j] = play[j];
 			play[j] = "";
 		}
+		//
 		dfsHelp(board2, play[1], 1, eval, play);
 		for (int j = 0; j < 3; j++)
 		{
@@ -214,6 +222,14 @@ void dfsMain2(CFBoard board)
 
 void update(int* eva, string* play, int e, string moo)
 {
+	/*
+	* @brief: updates the arrays
+	* @param eva: <int*> original array of evals
+	* @param play: <string*> original array of moves
+	* @param e: <int> eval of move to be checked
+	* @param moo: <string> string of moves to be checked
+	* @return the top 3 lines and their eval
+	*/
 	if (e > eva[2])
 	{
 		// we check if the naive eval is good and update it accordingly
@@ -244,14 +260,16 @@ void update(int* eva, string* play, int e, string moo)
 vector<int> allGoodMoves(CFBoard& board, int sq)
 {
 	/*
-	* input the board and the square of the piece which is going to make moves
-	* output all possible legal moves we want to play
+	* @brief: makes a vector of all good moves
+	* @param board: <CFBoard> current board.
+	* @param sq: <int> square index
+	* @return the vector of all good moves
 	*/
 	vector<int> moves;
 	int piece = board.getBit(sq);
 	int tot = numMoves(piece);
 	for (int j = 0; j < tot; j++)
-		if (isGood(board,sq,j))
+		if (board.isLegal(j))
 			moves.push_back(j);
 	return moves;
 }
@@ -259,8 +277,13 @@ vector<int> allGoodMoves(CFBoard& board, int sq)
 void dfsHelp(CFBoard &board, string moo, int depth, int* eva, string* play)
 {
 	/*
-	* input the board and the string of moves, the depth, the top 3 evaluations and their lines
-	* output at depth 5 the best ones
+	* @brief: helps the DFS
+	* @param board: <CFBoard> current board.
+	* @param moo: <string> current string of moves.
+	* @param depth: <int> current depth
+	* @param eva: <int*> pointer to array of top evaluations
+	* @param play: <play*> pointer to corresponding set of moves
+	* @return nothing
 	*/
 	// the actual DFS will start
 	if (depth == 5)
