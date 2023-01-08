e3394ed52af8a1d8201d37aac9c316cb389c4fd5
etienne-leroy <medieval511@gmail.com>
2023-01-08 12:59:13 +0100

    feat
    updated the Unit-Test main.cpp

--- draft/board_tests/main.cpp
+++ draft/board_tests/main.cpp
@@ -1,10 +1,28 @@
-#include "test_from_fen.h"
-#include "test_naive_check_check.h"
-#include "test_to_fen.h"
+#include "test_Board_Implementation.h"
 
 int main() {
-    CFBoardTester::testFromFEN();
-    CFBoardTester::testToFEN();
-    CFBoardTester::testNaiveCheckCheck();
+    CFBoardTester::test_pieceIdToChar();
+    CFBoardTester::test_pieceCharToId();
+    CFBoardTester::test_getColorBitBoard();
+    CFBoardTester::test_getPieceColorBitBoard();
+    CFBoardTester::test_getPieceBoardFromIndex();
+    CFBoardTester::test_getCurrentPlayer();
+    CFBoardTester::test_getPieceFromCoords();
+    CFBoardTester::test_getBit();
+    CFBoardTester::test_getMaterialCount();
+    CFBoardTester::test_addPiece();
+    CFBoardTester::test_removePiece();
+    CFBoardTester::test_movePiece();
+    CFBoardTester::test_forceUndo();
+    CFBoardTester::test_getCardinals();
+    CFBoardTester::test_getDiagonals();
+    CFBoardTester::test_getKnightPattern();
+    CFBoardTester::test_getKingPattern();
+    CFBoardTester::test_getPawnPattern();
+    CFBoardTester::test_getLegalMoves();
+    CFBoardTester::test_naiveCheckCheck();
+    CFBoardTester::test_fromFEN();
+    CFBoardTester::test_toFEN();
+
     return 0;
 }
