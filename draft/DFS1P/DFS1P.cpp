2659e1e5f0e5a9fae70abcc50e9a0fb3cfc0e19b
Ba Cong Vu <41003815+souhhcong@users.noreply.github.com>
2023-01-09 16:01:06 +0100

    feat: reduce running time of by not BFS from the same start tile

--- draft/DFS1P/DFS1P.cpp
+++ draft/DFS1P/DFS1P.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <queue>
 #include <tuple>
+#include <array>
+#include <algorithm>
 using std::cout;
 
 void DFS1P::setBoardPointer(CFBoard* ptr) {
@@ -19,19 +21,18 @@ bool squareSafeFromOpponentPawns(const bool &currentTurn, const uint64_t& oppone
 }
 
 //Distance between two squares with respect to a piece's movement
-int distFromTileToTileAsPiece(CFBoard& board, int halfPieceId, int startTile, int endTile) {
+std::array<std::array<int, 8>, 8> distFromTileToTilesAsPiece(CFBoard& board, int halfPieceId, int startTile) {
     std::queue<std::tuple<int,int>> q;
-    int dist[8][8];
+    std::array<std::array<int, 8>, 8> dist;
     bool currentTurn = board.getCurrentPlayer(); // 0: white, 1: black
     uint64_t opponentPawnBoard = board.getPieceColorBitBoard(!currentTurn);
-    memset(dist, -1, sizeof dist);
+    for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) dist[i][j] = -1;
     dist[startTile/8][startTile%8] = 0;
     q.push(std::make_tuple(startTile/8, startTile%8));
     // BFS to find shortest distance from startTile to endTile
     while(!q.empty()) {
         int curi = std::get<0>(q.front()), curj = std::get<1>(q.front());
         q.pop();
-        if (curi*8 + curj == endTile) return dist[curi][curj];
 
         uint64_t nextSquares = board.getLegalMoves(2*halfPieceId + currentTurn, curi*8 + curj);
         for (int newTile: bitSetPositions(nextSquares)) {
@@ -40,13 +41,13 @@ int distFromTileToTileAsPiece(CFBoard& board, int halfPieceId, int startTile, in
             if (!Heatmap::validSquare(newi, newj)) continue; // out of bound
             if (dist[newi][newj] != -1) continue; // already visited
             if (board.getPieceFromCoords(newi*8+newj) != -1) continue; // our piece or opponent piece already on that square
-            if (!squareSafeFromOpponentPawns(currentTurn, opponentPawnBoard, newi, newj)) continue;
+            if (!squareSafeFromOpponentPawns(currentTurn, opponentPawnBoard, newi, newj)) continue; // square unsafe for our piece
             // cout << newi << ' ' << newj << ' ' << (board.getPieceFromCoords(newi*8 + newj)) << '\n';
             dist[newi][newj] = dist[curi][curj] + 1;
             q.push(std::make_tuple(newi, newj));
         }
     }
-    return -1;
+    return dist;
 }
 
 int distFromHeatmap(CFBoard& board, int (&heatMap)[6][8][8]) {
@@ -55,12 +56,12 @@ int distFromHeatmap(CFBoard& board, int (&heatMap)[6][8][8]) {
     for (int halfPieceId = 0; halfPieceId < 6; halfPieceId++) {
         uint64_t pieceBoard = board.getPieceColorBitBoard(2*halfPieceId|currentTurn);
         std::vector<int> pieceTiles = bitSetPositions(pieceBoard);
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (heatMap[halfPieceId][i][j] != 0) {
-                    for (int startTile: pieceTiles) {
-                        if (distFromTileToTileAsPiece(board, halfPieceId, startTile, i*8+j) == -1) continue;
-                        dist += distFromTileToTileAsPiece(board, halfPieceId, startTile, i*8+j);
+        for (int startTile: pieceTiles) {
+            std::array<std::array<int, 8>, 8> distFromStart = distFromTileToTilesAsPiece(board, halfPieceId, startTile);
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (heatMap[halfPieceId][i][j] != 0 && distFromStart[i][j] != -1) {
+                        dist = distFromStart[i][j];
                     }
                 }
             }
@@ -85,6 +86,9 @@ void DFS1pAux(CFBoard* currentBoard, int depth, int maxDepth, std::vector<std::t
         std::vector<int> endTiles = bitSetPositions(legalMoves);
         for (int endTile: endTiles) {
             // std::cout << startTile << ' ' << endTile << '\n';
+            if (currentBoard->getPieceFromCoords(endTile) != -1) continue; // opponent pieces already on that square
+            if (!squareSafeFromOpponentPawns(currentTurn,
+            currentBoard->getPieceColorBitBoard(!currentTurn), endTile/8, endTile%8)) continue; // square unsafe for our piece
             curLine.push_back(std::make_tuple(startTile, endTile, 0.0));
             // cout << "current turn: " << currentTurn << '\n';
             currentBoard->movePiece(startTile, endTile);
@@ -105,7 +109,7 @@ std::tuple<int, int, float> DFS1P::getNextMove() {
 	uint64_t weakPawns = 8ll; // placeholder for finished weak pawns implementation
     Heatmap::addHeatMap(*currentBoard, heatMap, weakPawns);
 
-    int maxDepth = 2; // CUSTOMIZABLE MAX DEPTH FOR DFS, CAPPED AT 4 CURRENTLY
+    int maxDepth = 3; // CUSTOMIZABLE MAX DEPTH FOR DFS, CAPPED AT 4 CURRENTLY
     // Get all possible moves
     std::vector<std::tuple<int, int, float>> ansLine;
     std::vector<std::vector<std::tuple<int, int, float>>> possibleLines;
@@ -115,7 +119,9 @@ std::tuple<int, int, float> DFS1P::getNextMove() {
     DFS1pAux(currentBoard, 0, maxDepth, {}, possibleLines);
 
     for (auto line: possibleLines) {
+        // cout << "line: \n";
         for (auto move: line) {
+            // cout << "move: " << std::get<0>(move) << ' ' << std::get<1>(move) << '\n';
             // cout << "current turn: " << currentBoard->getCurrentPlayer() << '\n';
             // cout << "next move: " << std::get<0>(move) << ' ' << std::get<1>(move) << '\n';
             // cout << "current board: \n" << currentBoard->getRepr() << '\n'  << " current distance: " << distFromHeatmap(*currentBoard, heatMap) << '\n';
