d0f33c4cebd0fa623625257d3dddd1341052e1eb
D-2n <114923905+D-2n@users.noreply.github.com>
2023-01-12 10:00:53 +0100

    Piece recognition
    Single-instance piece recognition implemented + memory optimization

--- draft/cv_board/ConsoleApplication3/main.cpp
+++ draft/cv_board/ConsoleApplication3/main.cpp
@@ -6,6 +6,7 @@
 #include "class_decl.h"
 #include <Windows.h>
 #include <wingdi.h>
+#include <experimental/filesystem>
 using namespace cv;
 using namespace std;
 vector <Square> rects;
@@ -13,9 +14,9 @@ Scalar whitelow = cv::Scalar(30, 24, 233);
 Scalar whitehigh = cv::Scalar(36, 255, 255);
 Scalar blacklow = cv::Scalar(40, 102, 143);
 Scalar blackhigh = cv::Scalar(46, 255, 255);
-//getPosition takes as an imput a board object, player color and (x,y) coordinates on the board.
+//getPosition takes as an input a board object, player color and (x,y) coordinates on the board.
 //It then converts the coordinates to a chess board coordinate system.
-std::string getPosition(Board board, int x, int y,Player col) {
+std::string getPosition(Board &board, int x, int y,Player col) {
     char white[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
     char black[] = { 'h','g','f','e','d','c','b','a' };
     std::string position;
@@ -38,7 +39,7 @@ std::string getPosition(Board board, int x, int y,Player col) {
 }
 //findRects takes as imput the image and color boundaries for the chessboard square colors, and then it proceeds
 //to find the squares and return the overall board position on the screen.
-void findRects(cv::Mat img, cv::Scalar low, cv::Scalar high, cv::Scalar low1, cv::Scalar high1, COLOR color, COLOR color1,Board board) {
+void findRects(cv::Mat &img, cv::Scalar low, cv::Scalar high, cv::Scalar low1, cv::Scalar high1, COLOR color, COLOR color1,Board &board) {
     cv::Mat mask;
     cv::inRange(img, low, high, mask);
     std::vector<std::vector<cv::Point>> contours;
@@ -67,7 +68,7 @@ void findRects(cv::Mat img, cv::Scalar low, cv::Scalar high, cv::Scalar low1, cv
     }
 
 }
-void drawRects(cv::Mat img) { //draws highlight squares on the image of the board
+void drawRects(Mat &img) { //draws highlight squares on the image of the board
     for (size_t i = 0; i < rects.size(); ++i) {
         switch (rects[i].color) {
         case WHITE:
@@ -123,6 +124,7 @@ void addPieces(Board &board){
     board.Pieces.push_back(Piece('B', COLOR::BLACK, Tile('c', 8)));
     board.Pieces.push_back(Piece('B', COLOR::BLACK, Tile('f', 8)));
 }
+/* 
 void movePiece(string move, Board &board) {//executes the move, updates the board class
     //to do: Short and long castles, queen conversion
     //for some reason the main if isn't executed, check that
@@ -148,6 +150,7 @@ void movePiece(string move, Board &board) {//executes the move, updates the boar
     }
     board.Pieces = piecescopy;
 }
+*/
 bool use_mask;
 Mat img; Mat templ; Mat mask; Mat result;
 const char* image_window = "Source Image";
@@ -156,42 +159,61 @@ const char* result_window = "Result window";
 int match_method;
 int max_Trackbar = 5;
 
-void detectPieces(Board board, Mat img1) {//works for black only, not for all pawns
+void detectPieces(Board &board, Mat &img1, Mat &templ) {//works for black only, not for all pawns
     Mat img_orig;
-    cv::Mat templ = cv::imread("C:\\Users\\dimit\\Desktop\\cps\\black_king.png");
-    imshow("aa", templ);
+    img1.copyTo(img_orig);
     //Mat grey_orig;
     //Mat grey_template;
     //cvtColor(templ,grey_template, COLOR_BGR2GRAY);
     //cvtColor(img1, grey_orig, COLOR_BGR2GRAY);
     //grey_template.copyTo(img_orig);
     Mat result;
-    matchTemplate(img1, templ, result, TM_CCOEFF);
+    matchTemplate(img1, templ, result, TM_CCOEFF_NORMED);
     normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
     double minVal; double maxVal; Point minLoc; Point maxLoc;
     Point matchLoc;
     minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
     matchLoc = maxLoc;
-    rectangle(img_orig, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+    rectangle(img1, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
     rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-    //imshow(image_window, img_orig);
-    imshow(result_window, result);
-    
+    imshow(image_window, img_orig);
 }
 int  main() {  //when merging into the algo code change the name for the call
     //this func finds the board and initializes the main board class accordingly, storing all the data
     bool playerColor;
     Board board;
+    Mat black_king = imread("C:\\Users\\dimit\\Desktop\\cps\\black_king.png");
+    Mat black_queen = imread("C:\\Users\\dimit\\Desktop\\cps\\black_queen.png");
+    Mat black_knight = imread("C:\\Users\\dimit\\Desktop\\cps\\black_knight.png");
+    Mat black_bishop = imread("C:\\Users\\dimit\\Desktop\\cps\\black_bishop.png");
+    Mat black_rook = imread("C:\\Users\\dimit\\Desktop\\cps\\black_rook.png");
+    Mat black_pawn = imread("C:\\Users\\dimit\\Desktop\\cps\\black_pawn.png");
+    Mat white_king = imread("C:\\Users\\dimit\\Desktop\\cps\\white_king.png");
+    Mat white_queen = imread("C:\\Users\\dimit\\Desktop\\cps\\white_queen.png");
+    Mat white_knight = imread("C:\\Users\\dimit\\Desktop\\cps\\white_knight.png");
+    Mat white_bishop = imread("C:\\Users\\dimit\\Desktop\\cps\\white_bishop.png");
+    Mat white_rook = imread("C:\\Users\\dimit\\Desktop\\cps\\white_rook.png");
+    Mat white_pawn = imread("C:\\Users\\dimit\\Desktop\\cps\\white_pawn.png");
     //Takes the input image
     cv::Mat img = cv::imread("C:\\Users\\dimit\\Desktop\\scr3.png");
     cv::Mat orig;
     //Switches to HSV
     img.copyTo(orig);
+    //detectPieces(board, orig, black_queen);
+    detectPieces(board, orig, black_bishop);
+    detectPieces(board, orig, black_king);
+    detectPieces(board, orig, black_queen);
+    detectPieces(board, orig, white_king);
+    detectPieces(board, orig, white_queen);
+    detectPieces(board, orig, white_bishop);
+    detectPieces(board, orig, white_rook);
+    detectPieces(board, orig, white_knight);
+    detectPieces(board, orig, white_pawn);
     //cv::imshow("test", img);
     cv::cvtColor(img, img, cv::COLOR_BGR2HSV);
     //White field detection
     findRects(img, blacklow, blackhigh, whitelow, whitehigh, COLOR::BLACK, COLOR::WHITE, board);
-    drawRects(orig);
+    //drawRects(orig);
     for (int i = 0; i < rects.size(); i++) {
         if (rects[i].rec.tl().x < board.left) {
             board.left = rects[i].rec.tl().x;
@@ -226,13 +248,12 @@ int  main() {  //when merging into the algo code change the name for the call
     //adding tiles and pieces
     addTiles(playerColor, board);
     addPieces(board);
-    movePiece("Pa2a3", board);
     //
-    detectPieces(board, img);
     Point p2(board.left + 50, board.top+board.height/16);
     Point p4(board.left + 20, board.bottom - board.width / 16);
     rectangle(orig,p1,p2, Scalar(255, 0, 0) ,2,LINE_8); 
     rectangle(orig, p3, p4, Scalar(255, 0, 0), 2, LINE_8);
-    //cv::imshow("conts", orig);
+    namedWindow("aa", WINDOW_NORMAL);
+    imshow("aa", orig);
     cv::waitKey(0);
 }
