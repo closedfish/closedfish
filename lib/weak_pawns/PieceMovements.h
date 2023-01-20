904778954d40e77159d52c2dd0fc9bb389b3ac9c
Sirawit <plurm2545@hotmail.com>
2023-01-20 23:30:08 +0100

    fix: function decls

--- lib/weak_pawns/PieceMovements.h
+++ lib/weak_pawns/PieceMovements.h
@@ -3,10 +3,7 @@
 #include "WeakPawns.h"
 
 uint64_t getDangerousTiles(CFBoard board, bool color);
-uint64_t getTilesThatCanEat(CFBoard &board, bool &color);
-std::vector<int> CapturesFromLastMove(CFBoard &board, int lastMoveStart,
-																			int lastMoveEnd);
-bool isTileDangerous(CFBoard board, int tile);
-int canPieceMoveOut(CFBoard &board, int tile, int pieceId);
+bool isTileDangerous(CFBoard board, bool color, int tile);
+int canPieceMoveOut(CFBoard &board, int tile);
 uint64_t *getPieceMovements(CFBoard &board, bool &color, int tile);
 int getLastMove(CFBoard lastBoard, CFBoard currentBoard, bool colorPlayed);
