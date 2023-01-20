493e18150f30df11119d686f1ca564920487190a
Ba Cong Vu <41003815+souhhcong@users.noreply.github.com>
2023-01-21 00:41:27 +0100

    feat: add switch placeholder

--- lib/DFS1P/DFS1P.cpp
+++ lib/DFS1P/DFS1P.cpp
@@ -166,6 +166,14 @@ Closedfish::Move DFS1P::getNextMove() {
         if (cand2 == (int)player){return std::make_tuple(destination + backdirection*9,destination, 0.0);}
     }
 
+	float closedCoeff = 1.0;	// placeholder
+	float closedThreshold = 0.0;// for input from switch team
+	// Switch algo
+	if (closedCoeff <= closedThreshold) {
+		// Stockfish here
+		return std::make_tuple(0,0,0);
+	}
+
 	// Build the weak pawns
 	uint64_t weakPawns = 0;
 	int weakPawnsNumProtect = 1e9;
@@ -226,7 +234,6 @@ Closedfish::Move DFS1P::getNextMove() {
 			currentBoard->forceFlipTurn();
 		}
 	}
-	// cout << distFromHeatmap(*currentBoard, heatMap) << '\n';
 
 	// Return the first move in the potential line
 	return ansLine[0];
@@ -236,13 +243,14 @@ void DFS1P::testDFS() {
 	// CFBoard board = CFBoard("rkq1bnnr/2b2p1p/4pPpP/3pP1P1/p1pP2N1/PpP5/1P4K1/RNBQ1B1R w - - 0 1");
 	// CFBoard board = CFBoard("rkqrbnnb/8/p5p1/Pp1p1pPp/1PpPpP1P/2P1P1N1/2B1QB1R/3K3R w - - 0 1"); // no open files, >= 2 free rows
 	// CFBoard board = CFBoard("rkqr1nnb/4b3/8/p3p1p1/Pp1pPpPp/1PpP1P1P/R1P4N/1NKQBB1R w - - 0 1"); // no open files, 1 free rows, no chance of winning, 3 is better than 4 for some reasons
+	CFBoard board = CFBoard("rkqr1nnb/4b3/8/p3p1p1/Pp1pPpPp/1PpP1P1P/R1P4N/1NKQBB1R b - - 0 1"); // same as above but black to play
 	// CFBoard board = CFBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
 	// CFBoard board = CFBoard("2k5/p7/qp5p/1qp3pP/B1qp1pP1/3npPN1/P3PB1R/3KQ2R w - - 0 1"); // king supposed to have no legal moves
 	// std::cerr << board.getReprLegalMove(10, 59); 
 	// CFBoard board = CFBoard("2k5/p7/qp5p/1qp3pP/2qp1pP1/3npPN1/P1B1PB1R/3KQ2R w - - 0 1"); // test opponent blundering
 	// CFBoard board = CFBoard("2k5/p7/Pp5p/1Pp3pq/2Pp1pq1/3PpnN1/P1B1PB1R/3KQ2R w - - 0 1"); // test opponent blundering 2
 	// CFBoard board = CFBoard("2k5/p4q1q/Ppq1q1qp/1Ppq1qpq/2Ppqpq1/3PpnN1/P1B1PB1R/3KQ2R w - - 0 1"); // test opponent blundering 3
-	CFBoard board = CFBoard("2kqqqqq/p2qqq2/Pp5p/1Pp3pP/2Pp1NP1/3PpPB1/P1B1P2R/3KQ2R b - - 0 1"); // test opponent blundering 4
+	// CFBoard board = CFBoard("2kqqqqq/p2qqq2/Pp5p/1Pp3pP/2Pp1NP1/3PpPB1/P1B1P2R/3KQ2R b - - 0 1"); // test opponent blundering 4
 	// cout << board.naiveCheckCheck(0) << '\n';
 
 	setBoardPointer(&board);
