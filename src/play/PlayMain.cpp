d241ba0f80062a88eaf70274f1bda98b41aac6bc
Sirawit Pongnakintr <plurm2545@hotmail.com>
2023-01-21 22:31:47 +0100

    fix: migrate to vectors

--- src/play/PlayMain.cpp
+++ src/play/PlayMain.cpp
@@ -1,94 +1,62 @@
+#include "PlayMain.h"
 #include "GeneralRegression.h"
 #include <cmath>
-#include <iostream>
 #include <ctype.h>
+#include <iostream>
 
-
-
-int** convert_board(std::string fen)
-{	
-	int** ret = new int*[2];
+std::pair<std::vector<int>, std::vector<int>> convert_board(std::string fen) {
 	int sz = 0;
-	while (fen[sz] != ' ')
-	{
+	while (sz < fen.size() && fen[sz] != ' ') {
 		sz += 1;
 	}
 
 	std::string sub_fen = fen.substr(0, sz);
 
-	int* L_top = new int;
-	int* L_bottom = new int;
+	std::vector<int> L_top(8), L_bottom(8);
+	std::fill(L_top.begin(), L_top.end(), 8);
+	std::fill(L_bottom.begin(), L_bottom.end(), -1);
 
-	for (int j = 0; j < 8; j ++)
-	{
-		L_top[j] = 8;
-		L_bottom[j] = -1;	
-	}
-	
 	int counter = 0;
 	int row = 0;
 
-	for (int j = 0; j < sz; j ++)
-	{
+	for (int j = 0; j < sz; j++) {
 		char chess_piece = fen[j];
-		
 
-		if (chess_piece == '/')
-		{
+		if (chess_piece == '/') {
 			counter = 0;
 			row += 1;
 		}
 
-		else if (isdigit(chess_piece))
-			{
-				counter += (int)chess_piece - '0';
-			}
-		
-		else
-		{
-			if (chess_piece == 'p')
-			{
+		else if (isdigit(chess_piece)) {
+			counter += (int)chess_piece - '0';
+		}
+
+		else {
+			if (chess_piece == 'p') {
 				L_bottom[counter] = row;
 			}
 
-			if ((chess_piece == 'P') && (L_top[counter] == 8))
-			{
+			if ((chess_piece == 'P') && (L_top[counter] == 8)) {
 				L_top[counter] = row;
 			}
 
 			counter += 1;
 		}
-			
-		
 	}
-	
-	ret[0] = L_top;
-	ret[1] = L_bottom;
 
-	return ret;
-
-
+	return std::make_pair(L_top, L_bottom);
 }
 
-
-
-
-float closeness_evf(std::string fen)
-{
-	Func *basis = SqrtDifBasis::GenerateBasis();
-	int** board = convert_board(fen);
-	Eigen::VectorXd theta = EvaluationFunction::getTheta();
-	
-	int dimension = 23;
-	return EvaluationFunction::Evaluate(basis, theta, board[0],
-							 board[1], dimension);
-
-
+float evaluateOpenness(std::string fen) {
+	std::pair<std::vector<int>, std::vector<int>> board = convert_board(fen);
+	return EvaluationFunction::Evaluate(board.first, board.second, 23);
 }
 
-int main()
-{
-	std::cout << closeness_evf("rkq1bnnr/2b2p1p/4pPpP/3pP1P1/p1pP2N1/PpP3K1/1P6/RNBQ1B1R w - - 0 1");
+/*
+int main() {
+	EvaluationFunction::init();
+	std::cout << evaluateOpenness(
+			"rkq1bnnr/2b2p1p/4pPpP/3pP1P1/p1pP2N1/PpP3K1/1P6/RNBQ1B1R w - - 0 1");
 	return 0;
 }
-
+*/
